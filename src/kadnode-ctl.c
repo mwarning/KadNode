dfe8eae50d657941d14abf68ed6601e4617f3bfb
Moritz Warning <moritzwarning@web.de>
2013-06-25 20:46:10 +0200

    field is in microseconds

--- src/kadnode-ctl.c
+++ src/kadnode-ctl.c
@@ -44,7 +44,7 @@ int udp_send( char* buffer, int port ) {
 
 	/* Set receive timeout: 200ms */
 	tv.tv_sec = 0;
-	tv.tv_usec = 200;
+	tv.tv_usec = 200000;
 	if( setsockopt( sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv) ) < 0 ) {
 		fprintf( stderr, "Failed to set socket option SO_RCVTIMEO: %s\n", strerror( errno ) );
 		return 1;
