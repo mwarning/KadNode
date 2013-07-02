eab5cbf6c3bfef3c650fcdf5c3abe19c7b70890a
Moritz Warning <moritzwarning@web.de>
2013-07-03 01:51:55 +0200

    increase nodes that can be exported at once to 32

--- src/ext-cmd.c
+++ src/ext-cmd.c
@@ -131,7 +131,7 @@ int cmd_blacklist( REPLY *r, const char *addr_str ) {
 
 int cmd_export( REPLY *r ) {
 	char addrbuf[FULL_ADDSTRLEN+1];
-	IP addr_array[16];
+	IP addr_array[32];
 	int addr_num = N_ELEMS(addr_array);
 	int i;
 
