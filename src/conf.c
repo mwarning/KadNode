1f4cf45738628cea632691d1aeb71e3fd090a2bc
Moritz Warning <moritzwarning@web.de>
2013-09-23 21:38:49 +0200

    remove output of values from command line; only errors shall we printed when the arguments are parsed; we also now have the 'list values' command

--- src/conf.c
+++ src/conf.c
@@ -264,7 +264,6 @@ void conf_add_value( char *var, char *val ) {
 	/* Add new value */
 	id_compute( value_id, val );
 
-	log_info( "CFG: Add value %s:%d (based on '%s')", str_id( value_id, hexbuf ), port, val );
 	values_add( value_id, port, LONG_MAX );
 #ifdef FWD
 	forwardings_add( port, LONG_MAX );
