c4351b6240c1aa4f94ffda35d4840f0c614ef4a7
Moritz Warning <moritzwarning@web.de>
2014-07-30 14:25:59 +0200

    fix local multicast discovery for MACOSX; 10.8 and earlier does not implement MCAST_JOIN_GROUP correctly

--- src/ext-lpd.c
+++ src/ext-lpd.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <unistd.h> /* close */
+#include <sys/ioctl.h>
 
 #include "main.h"
 #include "conf.h"
@@ -34,10 +35,11 @@ static int g_sock_send = -1;
 
 
 int mcast_join_group( int sock, IP *addr, const char ifce[] ) {
+#if defined(MCAST_JOIN_GROUP) && !defined(__APPLE__)
 	struct group_req req;
 
 	if( ifce ) {
-		if( (req.gr_interface = htonl( if_nametoindex(ifce) )) == 0 ) {
+		if( (req.gr_interface = htonl( if_nametoindex( ifce ) )) == 0 ) {
 			log_err( "LPD: Cannot find interface '%s' for multicast: %s", ifce, strerror( errno ) );
 			return -1;
 		}
@@ -53,9 +55,60 @@ int mcast_join_group( int sock, IP *addr, const char ifce[] ) {
 	}
 
 	return 0;
+#else
+	switch( addr->ss_family ) {
+		case AF_INET: {
+			struct ip_mreq mreq;
+			struct ifreq ifreq;
+
+			memcpy( &mreq.imr_multiaddr, &((IP4*) addr)->sin_addr, 4 );
+
+			if( ifce ) {
+				strncpy( ifreq.ifr_name, ifce, IFNAMSIZ );
+
+				if( ioctl( sock, SIOCGIFADDR, &ifreq ) < 0 ) {
+					log_err( "LPD: Cannot find interface '%s' for multicast: %s", ifce, strerror( errno ) );
+					return -1;
+				}
+				memcpy( &mreq.imr_interface, &((struct sockaddr_in *) &ifreq.ifr_addr)->sin_addr, 4);
+			} else {
+				mreq.imr_interface.s_addr = htonl( INADDR_ANY );
+			}
+
+			if( setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0 ) {
+				log_warn( "LPD: Failed to join IPv4 multicast group: %s", strerror( errno ) );
+				return -1;
+			}
+			return 0;
+		}
+		case AF_INET6: {
+			struct ipv6_mreq	mreq6;
+
+			memcpy( &mreq6.ipv6mr_multiaddr, &((IP6*) addr)->sin6_addr, 16 );
+
+			if( ifce ) {
+				if( (mreq6.ipv6mr_interface = if_nametoindex( ifce )) == 0 ) {
+					log_err( "LPD: Cannot find interface '%s' for multicast: %s", ifce, strerror( errno ) );
+					return -1;
+				}
+			} else {
+				mreq6.ipv6mr_interface = 0;
+			}
+
+			if( setsockopt(sock, IPPROTO_IPV6, IPV6_JOIN_GROUP, &mreq6, sizeof(mreq6)) < 0 ) {
+				log_warn( "LPD: Failed to join IPv6 multicast group: %s", strerror( errno ) );
+				return -1;
+			}
+			return 0;
+		}
+		default:
+			return -1;
+	}
+#endif
 }
 
 int mcast_leave_group( int sock, IP *addr ) {
+#if defined(MCAST_JOIN_GROUP) && !defined(__APPLE__)
 	struct group_req req;
 
 	req.gr_interface = 0;
@@ -67,6 +120,34 @@ int mcast_leave_group( int sock, IP *addr ) {
 	}
 
 	return 0;
+#else
+	switch( addr->ss_family ) {
+		case AF_INET: {
+			struct ip_mreq mreq;
+
+			memcpy( &mreq.imr_multiaddr, &((IP4*) addr)->sin_addr, 4 );
+			mreq.imr_interface.s_addr = htonl( INADDR_ANY );
+			if( setsockopt( sock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq) ) < 0 ) {
+				log_warn( "LPD: Failed to leave IPv4 multicast group: %s", strerror( errno ) );
+				return -1;
+			}
+			return 0;
+		}
+		case AF_INET6: {
+			struct ipv6_mreq	mreq6;
+
+			memcpy( &mreq6.ipv6mr_multiaddr, &((IP6 *) addr)->sin6_addr, 16 );
+			mreq6.ipv6mr_interface = 0;
+			if( setsockopt( sock, IPPROTO_IPV6, IPV6_LEAVE_GROUP, &mreq6, sizeof(mreq6) ) < 0 ) {
+				log_warn( "LPD: Failed to leave IPv6 multicast group: %s", strerror( errno ) );
+				return -1;
+			}
+			return 0;
+		}
+		default:
+			return -1;
+	}
+#endif
 }
 
 const char *parse_packet_param( const char* str, const char* param ) {
