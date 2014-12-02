aea6e6d3811156f65939bf434cb01a9b6c20566f
Moritz Warning <moritzwarning@web.de>
2014-12-02 05:31:23 +0100

    update windows start/stop scripts

--- windows/kadnode-start.bat
+++ windows/kadnode-start.bat
@@ -1,40 +1,35 @@
-:: Set DNS settings and start KadNode so that KadNode can answer specific DNS queries
+:: Set DNS settings and start KadNode so that KadNode can answer specific DNS queries
+
 @ECHO OFF
 
-:: Check privileges
-net session >nul 2>&1
-if NOT %errorLevel% == 0 (
-  echo Failure: Need to be run as administrator.
-  exit /B 1
-)
 
-:: Set DNS
 SETLOCAL EnableDelayedExpansion
-
+
 FOR /F "tokens=* delims=:" %%a IN ('IPCONFIG ^| FIND /I "ETHERNET ADAPTER"') DO (
 SET adapterName=%%a
-
+
 REM Removes "Ethernet adapter" from the front of the adapter name
 SET adapterName=!adapterName:~17!
-
+
 REM Removes the colon from the end of the adapter name
 SET adapterName=!adapterName:~0,-1!
 
-REM Set fixed DNS server (Google) and KadNode as second
+REM Set fixed DNS server (Google) and KadNode as second
+
 netsh interface ipv4 add dns name="!adapterName!" 8.8.8.8 index=1
 netsh interface ipv4 add dns name="!adapterName!" 127.0.0.1 index=2
 netsh interface ipv6 add dns name="!adapterName!" 2001:4860:4860::8888 index=1
 netsh interface ipv6 add dns name="!adapterName!" ::1 index=2
 )
-
+
 ipconfig /flushdns
-
+
 :: Get script path
-SET path=%~dp0%
-path=!path:~0,-1!
+SET spath=%~dp0%
+SET spath=!spath:~0,-1!
 
 :: Kill running instances
-taskkill.exe /f /im kadnode.exe
+taskkill.exe /f /im kadnode.exe >nul 2>&1
 
 :: Start KadNode
-"!path!\kadnode.exe" --config "!path!\config.txt" --peerfile "!path!\peers.txt" --port 53 --daemon
+"!spath!\kadnode.exe" --config "!spath!\config.txt" --peerfile "!spath!\peers.txt" --port 53 --daemon
--- windows/kadnode-stop.bat
+++ windows/kadnode-stop.bat
@@ -2,43 +2,20 @@
 @ECHO OFF
 
 
-
-:: Check privileges
-net session >nul 2>&1
-if NOT %errorLevel% == 0 (
-  echo Failure: Need to be run as administrator.
-  exit /B 1
-)
-
 SETLOCAL EnableDelayedExpansion
 
 FOR /F "tokens=* delims=:" %%a IN ('IPCONFIG ^| FIND /I "ETHERNET ADAPTER"') DO (
 SET adapterName=%%a
 
-
 REM Removes "Ethernet adapter" from the front of the adapter name
-
 SET adapterName=!adapterName:~17!
 
-
-
-
 REM Removes the colon from the end of the adapter name
-
 SET adapterName=!adapterName:~0,-1!
 
-
-
-
 netsh interface ip set address "!adapterName!" dhcp
-
-netsh interface ip delete dnsserver "!adapterName!" all
 )
-
-
 
 ipconfig /flushdns
 
-
-
-taskkill.exe /f /im kadnode.exe
+taskkill.exe /f /im kadnode.exe >nul 2>&1
