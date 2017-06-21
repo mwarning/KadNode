a16251cd66502eb1d16d7e5fcc6e2d27aee18518
Moritz Warning <moritzwarning@web.de>
2017-06-21 23:22:52 +0200

    code style and refactoring

--- src/ext-tls-server.c
+++ src/ext-tls-server.c
@@ -30,6 +30,11 @@
 #include "ext-tls-server.h"
 
 
+/*
+* TLS server that closes the connection as soon as the handshake has been done.
+* The certificate are selected by Server Name Indication (SNI).
+*/
+
 static mbedtls_entropy_context g_entropy;
 static mbedtls_ctr_drbg_context g_drbg;
 static mbedtls_ssl_context g_ssl;
@@ -38,6 +43,8 @@ static mbedtls_ssl_config g_conf;
 static mbedtls_net_context g_listen_fd;
 static mbedtls_net_context g_client_fd;
 
+
+// Certificate for each domain we authenticate
 struct sni_entry {
 	const char *name;
 	mbedtls_x509_crt cert;
@@ -45,32 +52,33 @@ struct sni_entry {
 	struct sni_entry *next;
 };
 
-struct sni_entry *g_sni_entries = NULL;
+static struct sni_entry *g_sni_entries = NULL;
 
 
+// Forward declaration
 void tls_client_handler( int rc, int sock );
 
 
-void end_client_connection(int ret) {
-	/* No error checking, the connection might be closed already */
-	//do ret = mbedtls_ssl_close_notify( &ssl );
-	//while( ret == MBEDTLS_ERR_SSL_WANT_WRITE );
-	//ret = 0;
+void end_client_connection(int result) {
+	int ret;
+
+	// Done and close connection
+	do ret = mbedtls_ssl_close_notify( &g_ssl );
+	while( ret == MBEDTLS_ERR_SSL_WANT_WRITE );
 
 	mbedtls_net_free( &g_client_fd );
-
 	mbedtls_ssl_session_reset( &g_ssl );
 
-	if( ret == MBEDTLS_ERR_SSL_HELLO_VERIFY_REQUIRED )  {
-		log_info( "TLS:  hello verification requested" );
-	} else if( ret == MBEDTLS_ERR_SSL_CLIENT_RECONNECT ) {
-		log_info( "TLS: Client initiated reconnection from same port" );
-	} else if( ret != 0 ) {
+	if( result == MBEDTLS_ERR_SSL_HELLO_VERIFY_REQUIRED )  {
+		log_info( "TLS: Hello verification requested" );
+	} else if( result == MBEDTLS_ERR_SSL_CLIENT_RECONNECT ) {
+		log_debug( "TLS: Client initiated reconnection from same port" );
+	} else if( result != 0 ) {
 		char error_buf[100];
-		mbedtls_strerror( ret, error_buf, 100 );
-		log_info( "TLS: Last error was: %d - %s\n", ret, error_buf );
+		mbedtls_strerror( result, error_buf, 100 );
+		log_info( "TLS: Last error was: %d - %s\n", result, error_buf );
 	} else {
-		log_info( "TLS: all ok" );
+		log_info( "TLS: Authentication successful" );
 	}
 
 	net_remove_handler(g_listen_fd.fd, tls_client_handler);
@@ -78,11 +86,12 @@ void end_client_connection(int ret) {
 
 void tls_client_handler( int rc, int sock ) {
 	int ret;
+
 	log_info( "TLS: tls_client_handler, rc: %d\n", rc );
 
 	do ret = mbedtls_ssl_handshake( &g_ssl );
 	while( ret == MBEDTLS_ERR_SSL_WANT_READ ||
-		   ret == MBEDTLS_ERR_SSL_WANT_WRITE );
+		ret == MBEDTLS_ERR_SSL_WANT_WRITE );
 /*
 	int ret = mbedtls_ssl_handshake( &g_ssl );
 	if( ret == MBEDTLS_ERR_SSL_WANT_READ ||
@@ -100,14 +109,14 @@ void tls_client_handler( int rc, int sock ) {
 			int flags = mbedtls_ssl_get_verify_result( &g_ssl );
 			mbedtls_x509_crt_verify_info( vrfy_buf, sizeof( vrfy_buf ), "", flags );
 
-			log_debug( "TLS: Failed varfiy: %s", vrfy_buf );
+			log_debug( "TLS: Failed verfiy: %s", vrfy_buf );
 		}
 #endif
 		end_client_connection(ret);
 	} else {
 		// ret == 0
 		log_info( "TLS: Protocol is %s, Ciphersuite is %s",
-				mbedtls_ssl_get_version( &g_ssl ), mbedtls_ssl_get_ciphersuite( &g_ssl ) );
+			mbedtls_ssl_get_version( &g_ssl ), mbedtls_ssl_get_ciphersuite( &g_ssl ) );
 
 		int exp;
 		if( ( exp = mbedtls_ssl_get_record_expansion( &g_ssl ) ) >= 0 ) {
@@ -116,8 +125,8 @@ void tls_client_handler( int rc, int sock ) {
 			log_info( "TLS: Record expansion is unknown (compression)" );
 		}
 
-		// all ok
-		end_client_connection(ret);
+		// All ok
+		end_client_connection(0);
 	}
 }
 
@@ -139,7 +148,7 @@ void tls_server_handler( int rc, int sock ) {
 		return;
 	}
 
-	printf("TLS: got incoming connection\n");
+	log_debug("TLS: got incoming connection");
 
 	ret = mbedtls_net_set_nonblock( &g_client_fd );
 	if( ret != 0 ) {
@@ -203,13 +212,13 @@ void tls_add_sni_entry( const char name[], const char crt_file[], const char key
 	int ret;
 	if( (ret = mbedtls_x509_crt_parse_file( &new->cert, crt_file )) != 0 ) {     
 		mbedtls_strerror( ret, error_buf, sizeof(error_buf) );
-		log_err( "TLS: mbedtls_x509_crt_parse_file(%s) returned - %s", crt_file, error_buf );
+		log_err( "TLS: %s: %s", crt_file, error_buf );
 		exit( 1 );
 	}
 
 	if( (ret = mbedtls_pk_parse_keyfile( &new->key, key_file, "" /* no password */ )) != 0 ) {
 		mbedtls_strerror( ret, error_buf, sizeof(error_buf) );
-		log_err( "TLS: mbedtls_pk_parse_keyfile(%s) returned - %s", key_file, error_buf );
+		log_err( "TLS: %s: %s", key_file, error_buf );
 		exit( 1 );
 	}
 
@@ -231,27 +240,23 @@ void tls_server_setup( void ) {
 	mbedtls_ssl_config_init( &g_conf );
 	mbedtls_ctr_drbg_init( &g_drbg );
 
+	mbedtls_debug_set_threshold( 0 );
 
-mbedtls_debug_set_threshold( 0 );
-
- mbedtls_entropy_init( &g_entropy );
+	mbedtls_entropy_init( &g_entropy );
 	if( ( ret = mbedtls_ctr_drbg_seed( &g_drbg, mbedtls_entropy_func, &g_entropy,
-							   (const unsigned char *) pers,
-							   strlen( pers ) ) ) != 0 ) {
+		(const unsigned char *) pers, strlen( pers ) ) ) != 0 ) {
 		printf( "mbedtls_ctr_drbg_seed returned -0x%x\n", -ret );
 		return;
 	}
-
-	//tls_add_sni_entry( "foo.p2p", "data/device.crt", "data/device.key" );
 
 	if( g_sni_entries ) {
 		g_listen_fd.fd = net_bind( "TLS", gconf->dht_addr, "4433" /*gconf->dht_port*/, NULL, IPPROTO_TCP, AF_UNSPEC );
 
 		int ret;
 		if( ( ret = mbedtls_ssl_config_defaults( &g_conf,
-						MBEDTLS_SSL_IS_SERVER,
-						MBEDTLS_SSL_TRANSPORT_STREAM,
-						MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 )
+			MBEDTLS_SSL_IS_SERVER,
+			MBEDTLS_SSL_TRANSPORT_STREAM,
+			MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 )
 		{
 			log_err( "TLS: mbedtls_ssl_config_defaults returned -0x%x", -ret );
 			exit( 1 );
@@ -263,8 +268,7 @@ mbedtls_debug_set_threshold( 0 );
 
 		mbedtls_ssl_conf_sni( &g_conf, sni_callback, g_sni_entries );
 
-		if( ( ret = mbedtls_ssl_setup( &g_ssl, &g_conf ) ) != 0 )
-		{
+		if( ( ret = mbedtls_ssl_setup( &g_ssl, &g_conf ) ) != 0 ) {
 			log_err( "TLS: mbedtls_ssl_setup returned -0x%x", -ret );
 			exit( 1 );
 		}
--- src/ext-tls.c
+++ src/ext-tls.c
@@ -85,6 +85,7 @@ int tls_connect( mbedtls_ssl_context *ssl, mbedtls_net_context *fd, const char q
 	return 0;
 }
 
+// Find resource used by socket
 struct tls_resource *tls_find_resource( int fd ) {
 	int i;
 
@@ -98,14 +99,19 @@ struct tls_resource *tls_find_resource( int fd ) {
 }
 
 
-// forward declartion
+// Forward declaration
 void tls_handle( int rc, int fd );
 
 void auth_end( struct tls_resource* resource, int state ) {
 	struct results_t *results;
 	struct result_t *result;
+	int ret;
 
 	log_debug( "TLS: Auth %s for %s", (state == AUTH_OK) ? "success" : "failure", resource->results->query );
+
+	// Done and close connection
+	do ret = mbedtls_ssl_close_notify( &resource->ssl );
+	while( ret == MBEDTLS_ERR_SSL_WANT_WRITE );
 
 	result = resource->result;
 	results = resource->results;
@@ -122,7 +128,8 @@ void auth_end( struct tls_resource* resource, int state ) {
 	result->state = state;
 
 	if( state == AUTH_OK ) {
-		// Stop auth process for this 
+		// Stop authentication process for results
+		// since we found a verified entry
 		results->callback = NULL;
 
 		result = results->entries;
@@ -139,9 +146,9 @@ void auth_end( struct tls_resource* resource, int state ) {
 }
 
 void tls_handle( int rc, int fd ) {
-	struct tls_resource* resource; // = (struct tls_resource*) data;
-	mbedtls_ssl_context* ssl; // = &resource->ssl;
-	const char *query; // = resource->results->query;
+	struct tls_resource* resource;
+	mbedtls_ssl_context* ssl;
+	const char *query;
 	int ret;
 
 	resource = tls_find_resource( fd );
@@ -166,7 +173,7 @@ void tls_handle( int rc, int fd ) {
 			// Still connecting.
 		}
 	} else if( (ret = mbedtls_ssl_handshake( ssl ) ) == 0) {
-			// TLS handshake done
+		// TLS handshake done
 		log_debug( "TLS: Protocol [%s], Ciphersuite [%s] and fragment length %u: %s",
 			mbedtls_ssl_get_version( ssl ), mbedtls_ssl_get_ciphersuite( ssl ),
 			(unsigned int) mbedtls_ssl_get_max_frag_len( ssl ), query
@@ -177,28 +184,20 @@ void tls_handle( int rc, int fd ) {
 		uint32_t flags = 0;
 		char buf[MBEDTLS_SSL_MAX_CONTENT_LEN + 1];
 
-		if( ( flags = mbedtls_ssl_get_verify_result( ssl ) ) != 0 )
-		{
+		if( ( flags = mbedtls_ssl_get_verify_result( ssl ) ) != 0 ) {
 			mbedtls_x509_crt_verify_info( buf, sizeof( buf ), "", flags );
 			log_debug( "TLS: Peer verification failed:\n%s\n", buf);
 		}
 
-		if( mbedtls_ssl_get_peer_cert( ssl ) != NULL )
-		{
+		if( mbedtls_ssl_get_peer_cert( ssl ) != NULL ) {
 			mbedtls_x509_crt_info( (char *) buf, sizeof( buf ) - 1, "", mbedtls_ssl_get_peer_cert( ssl ) );
 			log_debug( "TLS: Peer certificate information:\n%s\n", buf);
 		}
 
-		// Done and close connection
-		do ret = mbedtls_ssl_close_notify( ssl );
-		while( ret == MBEDTLS_ERR_SSL_WANT_WRITE );
-
 		auth_end(resource, flags == 0 ? AUTH_OK : AUTH_FAILED);
-	}
-	else if( ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE )
-	{
+	} else if( ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE ) {
 		// TLS handshake failure.
-#ifdef DEBUG 
+#ifdef DEBUG
 		log_debug( "TLS: mbedtls_ssl_handshake returned -0x%x: %s", -ret, query );
 		if( ret == MBEDTLS_ERR_X509_CERT_VERIFY_FAILED ) {
 			log_debug( "TLS: Unable to verify the server's certificate: %s", query );
@@ -206,9 +205,7 @@ void tls_handle( int rc, int fd ) {
 #endif
 
 		auth_end(resource, AUTH_FAILED);
-	}
-	else
-	{
+	} else {
 		// TLS handshake in progress.
 	}
 }
@@ -230,6 +227,7 @@ struct tls_resource *tls_next_resource( void ) {
 	return NULL;
 }
 
+// Get next address to authenticate (state waiting)
 struct result_t *tls_next_result( struct results_t *results ) {
 	struct result_t *result;
 
@@ -251,15 +249,15 @@ void tls_trigger_auth( struct results_t *results ) {
 
 	result = results->entries;
 	while( result ) {
+		// Get next free SSL resource
 		resource = tls_next_resource();
 		if( resource == NULL ) {
-			// No SSL resources available
 			return;
 		}
 
+		// Get next result to authenticate
 		result = tls_next_result( results );
 		if( result == NULL ) {
-			// No results to authenticate
 			return;
 		}
 
@@ -280,6 +278,7 @@ void tls_trigger_auth( struct results_t *results ) {
 	}
 }
 
+#if DEBUG
 // Verifiy configuration
 int tls_conf_verify( void *data, mbedtls_x509_crt *crt, int depth, uint32_t *flags ) {
 	char buf[MBEDTLS_SSL_MAX_CONTENT_LEN + 1];
@@ -298,6 +297,7 @@ int tls_conf_verify( void *data, mbedtls_x509_crt *crt, int depth, uint32_t *fla
 
 	return 0;
 }
+#endif
 
 // Load the trusted CA
 void tls_add_ca_entry( const char ca_path[] ) {
@@ -311,50 +311,7 @@ void tls_add_ca_entry( const char ca_path[] ) {
 		exit(1);
 	}
 
-	log_info( "TLS: Loaded certificates from: %s (%d skipped)", ret );
-}
-
-int auth_conf( /*const char ca_path[]*/ ) {
-	int ret;
-	int i;
-/*
-	if( ca_path == NULL ) {
-		ca_path = "/usr/share/ca-certificates/mozilla/";
-	}
-
-	// Load the trusted CA
-	log_info( "Loading the CA root certificate from: %s", ca_path );
-
-	ret = mbedtls_x509_crt_parse_path( &g_cacert, ca_path );
-	log_info( "TLS: Loaded certificates: ok (%d skipped)", ret );
-*/
-	// Setting up the SSL/TLS structure.
-	if( ( ret = mbedtls_ssl_config_defaults( &g_conf,
-		MBEDTLS_SSL_IS_CLIENT,
-		MBEDTLS_SSL_TRANSPORT_STREAM,
-		MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 )
-	{
-		printf( " failed\n  ! mbedtls_ssl_config_defaults returned -0x%x\n\n", -ret );
-		return -1;
-	}
-
-#ifdef DEBUG
-	mbedtls_ssl_conf_verify( &g_conf, tls_conf_verify, NULL );
-#endif
-
-	mbedtls_ssl_conf_rng( &g_conf, mbedtls_ctr_drbg_random, &g_drbg );
-	mbedtls_ssl_conf_read_timeout( &g_conf, 0 );
-	mbedtls_ssl_conf_ca_chain( &g_conf, &g_cacert, NULL );
-
-	for( i = 0; i < N_ELEMS(g_tls_resources); i++ ) {
-		if( ( ret = mbedtls_ssl_setup( &g_tls_resources[i].ssl, &g_conf ) ) != 0 )
-		{
-			log_err( "TLS: mbedtls_ssl_setup returned -0x%x\n\n", -ret );
-			return -1;
-		}
-	}
-
-	return 0;
+	log_info( "TLS: Loaded certificates from: %s (%d skipped)", ca_path, ret );
 }
 
 // TODO: create server IP address on the same port as the DHT.
@@ -376,8 +333,8 @@ void tls_setup( void ) {
 	mbedtls_ctr_drbg_init( &g_drbg );
 	mbedtls_entropy_init( &g_entropy );
 
-	if( ( ret = mbedtls_ctr_drbg_seed( &g_drbg, mbedtls_entropy_func, &g_entropy, (const unsigned char *) pers, strlen( pers ) ) ) != 0 )
-	{
+	if( ( ret = mbedtls_ctr_drbg_seed( &g_drbg, mbedtls_entropy_func, &g_entropy,
+		(const unsigned char *) pers, strlen( pers ) ) ) != 0 ) {
 		log_err( "TLS: mbedtls_ctr_drbg_seed returned -0x%x\n", -ret );
 		exit(1);
 		return;
@@ -391,7 +348,29 @@ void tls_setup( void ) {
 	mbedtls_ssl_config_init( &g_conf );
 	mbedtls_x509_crt_init( &g_cacert );
 
-	auth_conf( /*gconf->tls_path*/ );
+	// Setting up the SSL/TLS structure.
+	if( ( ret = mbedtls_ssl_config_defaults( &g_conf,
+		MBEDTLS_SSL_IS_CLIENT,
+		MBEDTLS_SSL_TRANSPORT_STREAM,
+		MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 ) {
+		log_err( "TLS: mbedtls_ssl_config_defaults returned -0x%x\n\n", -ret );
+		exit(1);
+	}
+
+#ifdef DEBUG
+	mbedtls_ssl_conf_verify( &g_conf, tls_conf_verify, NULL );
+#endif
+
+	mbedtls_ssl_conf_rng( &g_conf, mbedtls_ctr_drbg_random, &g_drbg );
+	mbedtls_ssl_conf_read_timeout( &g_conf, 0 );
+	mbedtls_ssl_conf_ca_chain( &g_conf, &g_cacert, NULL );
+
+	for( i = 0; i < N_ELEMS(g_tls_resources); i++ ) {
+		if( ( ret = mbedtls_ssl_setup( &g_tls_resources[i].ssl, &g_conf ) ) != 0 ) {
+			log_err( "TLS: mbedtls_ssl_setup returned -0x%x\n\n", -ret );
+			exit(1);
+		}
+	}
 }
 
 void tls_free( void ) {
