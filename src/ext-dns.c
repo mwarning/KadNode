43bcb56314ab45b0403999df45d167cf1c2dcb94
Moritz Warning <moritzwarning@web.de>
2017-08-06 23:40:44 +0200

    fix af check in ext-dns

--- src/ext-dns.c
+++ src/ext-dns.c
@@ -649,18 +649,21 @@ void dns_handler( int rc, int sock ) {
 		return;
 	}
 
-	if( msg.question.qType == A_Resource_RecordType && gconf->af != AF_INET ) {
-		log_debug( "DNS: Received request for IPv4 record (A), but DHT uses IPv6." );
-		return;
-	}
+	const int af = gconf->af;
+	if( af != AF_UNSPEC ) {
+		if( msg.question.qType == A_Resource_RecordType && af != AF_INET ) {
+			log_debug( "DNS: Received request for IPv4 record (A), but DHT uses IPv6 only." );
+			return;
+		}
 
-	if( msg.question.qType == AAAA_Resource_RecordType && gconf->af != AF_INET6 ) {
-		log_debug( "DNS: Received request for IPv6 record (AAAA), but DHT uses IPv4." );
-		return;
+		if( msg.question.qType == AAAA_Resource_RecordType && af != AF_INET6 ) {
+			log_debug( "DNS: Received request for IPv6 record (AAAA), but DHT uses IPv4 only." );
+			return;
+		}
 	}
 
 	if ( !str_isValidHostname( hostname ) ) {
-		log_warn( "DNS: Invalid hostname for lookup: '%s'", hostname );
+		log_warn( "DNS: Invalid hostname for lookup: %s", hostname );
 		return;
 	}
 
