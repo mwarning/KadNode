9126ca7dedaf27885b24a628bd2771ca9b8ae686
Moritz Warning <moritzwarning@web.de>
2017-08-17 02:38:49 +0200

    cleanup kadnode-ctl code

--- src/kadnode-ctl.c
+++ src/kadnode-ctl.c
@@ -13,86 +13,33 @@
 
 #include "main.h"
 
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
 #define BUFSIZE 1472
 
 
 const char *usage = MAIN_SRVNAME" Control Program - Send commands to a KadNode instance.\n\n"
 "Usage: kadnode-ctl [OPTIONS]* [COMMANDS]*\n"
 "\n"
-" -p <port>	Connect to this port (Default: "CMD_PORT")\n"
+" -p <port>	Connect to this port (Default: "STR(CMD_PORT)")\n"
 " -h		Print this help.\n"
 "\n";
 
 
-/* Parse a port - treats 0 as valid port */
-int port_parse( const char *pstr, int err ) {
+// Parse a port - treats 0 as valid port
+int port_parse( const char pstr[], int err ) {
 	int port;
-	size_t i;
 
-	port = 0;
-	for( i = 0; pstr[i] != '\0'; i++ ) {
-		if( pstr[i] < '0' || pstr[i] > '9' ) {
-			return err;
-		}
-		port *= 10;
-		port += pstr[i] - '0';
-	}
-
-	if( i == 0 || port < 0 || port > 65535 ) {
-		return err;
-	} else {
+	if( sscanf( pstr, "%d", &port ) == 1 && port >= 0 && port <= 65535 ) {
 		return port;
+	} else {
+		return err;
 	}
 }
 
-int addr_len( const IP *addr ) {
-	switch( addr->ss_family ) {
-		case AF_INET:
-			return sizeof(IP4);
-		case AF_INET6:
-			return sizeof(IP6);
-		default:
-			return 0;
-	}
-}
-
-/*
-* Parse/Resolve an IP address.
-* The port must be specified separately.
-*/
-int addr_parse( IP *addr, const char addr_str[], const char port_str[], int af ) {
-	struct addrinfo hints;
-	struct addrinfo *info = NULL;
-	struct addrinfo *p = NULL;
-
-	memset( &hints, '\0', sizeof(struct addrinfo) );
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_family = af;
-
-	if( getaddrinfo( addr_str, port_str, &hints, &info ) != 0 ) {
-		return -2;
-	}
-
-	p = info;
-	while( p != NULL ) {
-		if( p->ai_family == AF_INET6 ) {
-			memcpy( addr, p->ai_addr, sizeof(IP6) );
-			freeaddrinfo( info );
-			return 0;
-		}
-		if( p->ai_family == AF_INET ) {
-			memcpy( addr, p->ai_addr, sizeof(IP4) );
-			freeaddrinfo( info );
-			return 0;
-		}
-	}
-
-	freeaddrinfo( info );
-	return -3;
-}
-
-/* Read from socket with timeout */
-int select_read( int sockfd, char *buffer, int bufsize, struct timeval *tv ) {
+// Read from socket with timeout
+int select_read( int sockfd, char buffer[], int bufsize, struct timeval *tv ) {
 	fd_set rfds;
 	int retval;
 
@@ -102,35 +49,34 @@ int select_read( int sockfd, char *buffer, int bufsize, struct timeval *tv ) {
 	retval = select( sockfd + 1, &rfds, NULL, NULL, tv );
 
 	if( retval == -1 ) {
-		/* Error */
+		// Error
 		return -1;
 	} else if( retval ) {
-		/* Data available */
+		// Data available
 		return read( sockfd, buffer, bufsize );
 	} else {
-		/* Timeout reached */
+		// Timeout reached
 		return 0;
 	}
 }
 
-int udp_send( char buffer[], const char port[] ) {
+int udp_send( char buffer[], uint8_t port ) {
 	struct timeval tv;
-	IP sockaddr;
+	IP6 sockaddr;
 	socklen_t addrlen;
 	int sockfd;
 	int n;
 
-	if( addr_parse( &sockaddr, "::1", CMD_PORT, AF_UNSPEC ) < 0 ) {
-		fprintf( stderr, "Failed to get localhost address: %s\n", strerror( errno ) );
-		return 1;
-	}
+	sockaddr.sin6_family = AF_INET6;
+	inet_pton( AF_INET6, "::1", &sockaddr.sin6_addr);
+	sockaddr.sin6_port = htons( port );
 
-	if( (sockfd = socket( sockaddr.ss_family, SOCK_DGRAM, IPPROTO_UDP )) < 0 ) {
+	if( (sockfd = socket( sockaddr.sin6_family, SOCK_DGRAM, IPPROTO_UDP )) < 0 ) {
 		fprintf( stderr, "Failed to create socket: %s\n", strerror( errno ) );
 		return 1;
 	}
 
-	addrlen = addr_len( &sockaddr );
+	addrlen = sizeof( sockaddr );
 	if( sendto( sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&sockaddr, addrlen ) < 0 ) {
 		fprintf( stderr, "Cannot connect to server: %s\n", strerror( errno ) );
 		return 1;
@@ -163,10 +109,10 @@ int udp_send( char buffer[], const char port[] ) {
 	close( sockfd );
 
 	if( buffer[0] == '0' ) {
-		fprintf( stdout, "%s", buffer+1 );
+		fprintf( stdout, "%s", buffer + 1 );
 		return 0;
 	} else if( buffer[0] == '1' ) {
-		fprintf( stderr, "%s", buffer+1 );
+		fprintf( stderr, "%s", buffer + 1 );
 		return 1;
 	} else {
 		fprintf( stderr, "Invalid response received.\n" );
@@ -177,12 +123,12 @@ int udp_send( char buffer[], const char port[] ) {
 int main( int argc, char **argv ) {
 	char buffer[BUFSIZE];
 	size_t i;
-	const char *port;
+	uint16_t port;
 
-	/* Use the default port */
+	// Use the default port
 	port = CMD_PORT;
 
-	/* Skip binary name */
+	// Skip program name
 	argc -= 1;
 	argv += 1;
 
@@ -192,8 +138,8 @@ int main( int argc, char **argv ) {
 			return 0;
 		} else if( strcmp( argv[0], "-p" ) == 0 ) {
 			if( argc >= 2 ) {
-				port = argv[1];
-				/* Skip option and port */
+				port = port_parse( argv[1], -1 );
+				// Skip option and port
 				argc -= 2;
 				argv += 2;
 			} else {
@@ -203,12 +149,12 @@ int main( int argc, char **argv ) {
 		}
 	}
 
-	if( port_parse( port, -1 ) < 1 ) {
+	if( port < 1 ) {
 		fprintf( stderr, "Port is invalid!\n" );
 		return 1;
 	}
 
-	/* Construct request string from args */
+	// Construct request string from args
 	buffer[0] = '\0';
 	for( i = 0; i < argc; ++i ) {
 		strcat( buffer, " " );
