b83c7367809f60e682ae3690399062e72896c4b5
Moritz Warning <moritzwarning@web.de>
2017-08-19 01:38:16 +0200

    add first try to have kadnode as a library

---
+++ src/libkadnode.h
@@ -0,0 +1,16 @@
+
+#ifndef _LIBKADNODE_H_
+#define _LIBKADNODE_H_
+
+/*
+* KadNode as a library.
+*
+* Not yet finished!
+*/
+
+int kadnode_init( void );
+int kadnode_set( const char opt[], const char val[] );
+int kadnode_lookup( const char query[], struct sockaddr_storage addr_array[], size_t addr_num );
+void kadnode_stop( void );
+
+#endif // _LIBKADNODE_H_
