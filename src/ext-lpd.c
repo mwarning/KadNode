84cec60f032e4007e9d1b734f8489e3661283998
Moritz Warning <moritzwarning@web.de>
2017-08-23 00:01:16 +0200

    fix some type warnings

--- src/ext-lpd.c
+++ src/ext-lpd.c
@@ -56,7 +56,7 @@ static void handle_mcast( int rc, struct LPD_STATE* lpd ) {
 		// No peers known, send multicast
 		if( kad_count_nodes( 0 ) == 0 ) {
 			log_debug( "LPD: Try to send hello to %s", str_addr( &lpd->mcast_addr ) );
-			sprintf( buf, "DHT %hu", gconf->dht_port );
+			sprintf( buf, "DHT %d", gconf->dht_port );
 			sendto( lpd->sock_send, (void const*) buf, strlen(buf), 0, (struct sockaddr const*) &lpd->mcast_addr, sizeof(IP) );
 		}
 
--- src/peerfile.c
+++ src/peerfile.c
@@ -32,7 +32,8 @@ static struct peer *g_peers = NULL;
 void peerfile_export( void ) {
 	const char *filename;
 	IP addrs[200];
-	size_t i, num;
+	size_t i;
+	int num;
 	FILE *fp;
 
 	filename = gconf->peerfile;
@@ -47,13 +48,8 @@ void peerfile_export( void ) {
 
 	num = kad_export_nodes( addrs, N_ELEMS(addrs) );
 
-	if( num < 0 ) {
-		log_warn("PEERFILE: Failed to export nodes.");
-		return;
-	}
-
 	// No peers to export
-	if( num == 0 ) {
+	if( num <= 0 ) {
 		log_info( "PEERFILE: No peers to export." );
 		return;
 	}
