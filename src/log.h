1b3e8894ebd85bb4af1864d1a714ae49cf49f4ca
Moritz Warning <moritzwarning@web.de>
2018-01-31 01:27:26 +0100

    use kernel style for code defines

--- src/log.h
+++ src/log.h
@@ -10,17 +10,33 @@
 #define VERBOSITY_VERBOSE LOG_INFO
 #define VERBOSITY_QUIET LOG_WARNING
 
-#define log_err(...)  { log_print( LOG_ERR, __VA_ARGS__ ); }
-#define log_info(...) if( gconf->verbosity >= LOG_INFO ) { log_print( LOG_INFO, __VA_ARGS__ ); }
-#define log_warn(...) if( gconf->verbosity >= LOG_WARNING ) { log_print( LOG_WARNING, __VA_ARGS__ ); }
+
+#define log_error(...) \
+  log_print(LOG_ERR, __VA_ARGS__);
+
+#define log_info(...)                   \
+  do {                                  \
+    if (g_config.verbosity >= LOG_INFO) \
+      log_print(LOG_INFO, __VA_ARGS__); \
+  } while (0)
+
+#define log_warning(...)                   \
+  do {                                     \
+    if (g_config.verbosity >= LOG_WARNING) \
+      log_print(LOG_WARNING, __VA_ARGS__); \
+  } while (0)
+
 #ifdef DEBUG
-#define log_debug(...) if( gconf->verbosity >= LOG_DEBUG ) { log_print( LOG_DEBUG, __VA_ARGS__ ); }
+  #define log_debug(...)                   \
+    do {                                   \
+      if (g_config.verbosity >= LOG_DEBUG) \
+        log_print(LOG_DEBUG, __VA_ARGS__); \
+    } while (0)
 #else
-#define log_debug(...) // Exclude debug messages from debug build
+  #define log_debug(...) // Exclude debug messages from debug build
 #endif
 
 // Print a log message
-void log_print( int priority, const char format[], ... );
-
+void log_print(int priority, const char format[], ...);
 
 #endif // _LOG_H_
