4c28b58d4f4d6aa4f787aca7f8a2a780858b2f61
Moritz Warning <moritzwarning@web.de>
2019-05-20 16:50:36 +0200

    add missing next in loop

--- src/ext-dns.c
+++ src/ext-dns.c
@@ -512,6 +512,7 @@ static int dns_setup_msg(struct Message *msg, const struct result_t *results, co
 
 		for (result = results, i = 0; result && c < MAX_ADDR_RECORDS; result = result->next) {
 			if (is_valid_result(result)) {
+				//g_names[i++] => qName and i++
 				setAddressRecord(&msg->answers[c++], g_names[i++], &result->addr);
 				msg->anCount++;
 			}
